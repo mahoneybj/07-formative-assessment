f839ba9379007c1f2f3f133fd21d17941a962483
mahoneybj <benmahoney10@gmail.com>
2023-09-18 16:17:19 +1200

    Done formative 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,345 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            button1 = new Button();
+            label11 = new Label();
+            label12 = new Label();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(39, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(199, 155);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(522, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(199, 155);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(84, 219);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Pizza size:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(84, 331);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 28);
+            label2.TabIndex = 4;
+            label2.Text = "Extras:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(255, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(250, 33);
+            label3.TabIndex = 5;
+            label3.Text = "Polly's Pizza Parlour";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(223, 228);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(67, 25);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Small";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(223, 257);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 25);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Large";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox1.Location = new Point(216, 337);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(100, 25);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Anchovies";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox2.Location = new Point(216, 362);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(114, 25);
+            checkBox2.TabIndex = 9;
+            checkBox2.Text = "Extra cheese";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox3.Location = new Point(216, 387);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(72, 25);
+            checkBox3.TabIndex = 10;
+            checkBox3.Text = "Olives";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox4.Location = new Point(216, 412);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(113, 25);
+            checkBox4.TabIndex = 11;
+            checkBox4.Text = "Mushrooms";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox5.Location = new Point(216, 437);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(99, 25);
+            checkBox5.TabIndex = 12;
+            checkBox5.Text = "Pepperoni";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(321, 230);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 21);
+            label4.TabIndex = 13;
+            label4.Text = "$5.00";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(312, 261);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 21);
+            label5.TabIndex = 14;
+            label5.Text = "$10.00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(339, 338);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 21);
+            label6.TabIndex = 15;
+            label6.Text = ".50";
+            label6.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(321, 363);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 21);
+            label7.TabIndex = 16;
+            label7.Text = "$1.00";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(330, 388);
+            label8.Name = "label8";
+            label8.Size = new Size(40, 21);
+            label8.TabIndex = 17;
+            label8.Text = "0.75";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(339, 413);
+            label9.Name = "label9";
+            label9.Size = new Size(31, 21);
+            label9.TabIndex = 18;
+            label9.Text = ".50";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(321, 438);
+            label10.Name = "label10";
+            label10.Size = new Size(49, 21);
+            label10.TabIndex = 19;
+            label10.Text = "$1.50";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(216, 503);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 36);
+            button1.TabIndex = 20;
+            button1.Text = "ORDER PIZZA";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(417, 230);
+            label11.Name = "label11";
+            label11.Size = new Size(97, 28);
+            label11.TabIndex = 21;
+            label11.Text = "Pizza size:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(417, 434);
+            label12.Name = "label12";
+            label12.Size = new Size(97, 28);
+            label12.TabIndex = 22;
+            label12.Text = "Pizza size:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(550, 443);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 24;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(550, 230);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 25;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(15F, 37F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(568, 368);
+            ClientSize = new Size(758, 551);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(button1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Button button1;
+        private Label label11;
+        private Label label12;
+        private TextBox textBox2;
+        private ListBox listBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,39 @@ namespace Formative_assessment_07
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox2.Text = string.Empty;
+
+            double total = 0;
+
+            if (radioButton1.Checked)
+            {
+                listBox1.Items.Add("Small pizza with:");
+                total += 5;
+            }
+            else
+            {
+                listBox1.Items.Add("Large pizza with:");
+                total += 10;
+            }
+
+            if (checkBox1.Checked) { total += 0.5; listBox1.Items.Add("Anchovies"); }
+            if (checkBox2.Checked) { total += 1; listBox1.Items.Add("Extra cheese"); }
+            if (checkBox3.Checked) { total += 0.75; listBox1.Items.Add("Olives"); }
+            if (checkBox4.Checked) { total += 0.5; listBox1.Items.Add("Mushrooms"); }
+            if (checkBox5.Checked) { total += 1.5; listBox1.Items.Add("Pepperoni"); }
+
+
+            textBox2.Text = Convert.ToString(total);
+
+
+
+
+        }
+
+
     }
 }
